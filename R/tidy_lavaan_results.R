1ede403544b62d274920f31c431d30dde809d747
Brian Anderson <drbanderson.cu@gmail.com>
2018-05-24 09:18:27 -0500

    Missed the pipe!

--- R/tidy_lavaan_results.R
+++ R/tidy_lavaan_results.R
@@ -29,7 +29,7 @@ tidy_lavaan <- function(x) {
     # Drop unlabeled covariances only if the 'label' column is present
     {if("label" %in% names(.)) filter(., !(op == "~~" & label == "")) else .} %>%
     # Drop the intercepts
-    filter(!op == "~1")
+    filter(!op == "~1") %>%
     mutate(est = round(est, 2),
            se = round(se, 2),
            pvalue = round(pvalue, 3)) %>%
