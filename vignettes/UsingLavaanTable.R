6f0e2b87bc2ab61d2bf6c99d21976133e04295b9
Brian Anderson <drbanderson.cu@gmail.com>
2018-06-05 14:45:12 -0500

    Started a using lavaanTable vignette

---
+++ vignettes/UsingLavaanTable.R
@@ -0,0 +1,28 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(collapse = TRUE, comment = "#>", message = FALSE, warning = FALSE)
+
+## ------------------------------------------------------------------------
+library(lavaan)
+model <- '
+  # measurement model
+    ind60 =~ x1 + x2 + x3
+    dem60 =~ y1 + y2 + y3 + y4
+    dem65 =~ y5 + y6 + y7 + y8
+  # regressions
+    dem60 ~ ind60
+    dem65 ~ ind60 + dem60
+  # residual correlations
+    y1 ~~ y5
+    y2 ~~ y4 + y6
+    y3 ~~ y7
+    y4 ~~ y8
+    y6 ~~ y8
+'
+
+## ------------------------------------------------------------------------
+fit <- sem(model, data = PoliticalDemocracy)
+summary(fit)
+
+## ------------------------------------------------------------------------
+
+
